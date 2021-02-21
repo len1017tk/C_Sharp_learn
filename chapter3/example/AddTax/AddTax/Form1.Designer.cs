967af17dfdf14f33e46ceea4db59bdd332a4d0cd
Takeshi Kawamura <sukimachatlen@gmail.com>
2021-02-21 23:58:35 +0900

    add chapter3/example/addTax/

---
+++ chapter3/example/AddTax/AddTax/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace AddTax
+{
+    partial class FormAddTax
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxManey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelAddTax = new System.Windows.Forms.Label();
+            this.buttonAddTax = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "金額";
+            // 
+            // textBoxManey
+            // 
+            this.textBoxManey.Location = new System.Drawing.Point(16, 32);
+            this.textBoxManey.Name = "textBoxManey";
+            this.textBoxManey.Size = new System.Drawing.Size(100, 19);
+            this.textBoxManey.TabIndex = 1;
+            this.textBoxManey.Text = "0";
+            this.textBoxManey.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "円";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "税込金額";
+            // 
+            // labelAddTax
+            // 
+            this.labelAddTax.AutoSize = true;
+            this.labelAddTax.Location = new System.Drawing.Point(169, 35);
+            this.labelAddTax.Name = "labelAddTax";
+            this.labelAddTax.Size = new System.Drawing.Size(23, 12);
+            this.labelAddTax.TabIndex = 4;
+            this.labelAddTax.Text = "0円";
+            // 
+            // buttonAddTax
+            // 
+            this.buttonAddTax.Location = new System.Drawing.Point(81, 70);
+            this.buttonAddTax.Name = "buttonAddTax";
+            this.buttonAddTax.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddTax.TabIndex = 5;
+            this.buttonAddTax.Text = "消費税追加";
+            this.buttonAddTax.UseVisualStyleBackColor = true;
+            this.buttonAddTax.Click += new System.EventHandler(this.buttonAddTax_Click);
+            // 
+            // FormAddTax
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 107);
+            this.Controls.Add(this.buttonAddTax);
+            this.Controls.Add(this.labelAddTax);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxManey);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAddTax";
+            this.Text = "消費税追加";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxManey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelAddTax;
+        private System.Windows.Forms.Button buttonAddTax;
+    }
+}
+
---
+++ chapter3/example/AddTax/AddTax/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddTax
+{
+    public partial class FormAddTax : Form
+    {
+        public FormAddTax()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddTax_Click(object sender, EventArgs e)
+        {
+            int money;
+            double addTax;
+            const double Tax = 0.1;
+
+            money = int.Parse(textBoxManey.Text);
+
+            addTax = money;
+            addTax *= (1 + Tax);
+            money = (int)addTax;
+
+            labelAddTax.Text = money + " 円";
+        }
+    }
+}
